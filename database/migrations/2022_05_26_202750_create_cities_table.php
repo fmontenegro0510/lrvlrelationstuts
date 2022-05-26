45bf85a9900d3328695c625852f600d295df8e91
fmontenegro0510 <franciscomontenegro22@gmail.com>
2022-05-26 19:18:22 -0300

    edited id table

--- database/migrations/2022_05_26_202750_create_cities_table.php
+++ database/migrations/2022_05_26_202750_create_cities_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('cities', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->unsignedInteger('country_id')->nullable();
             $table->string('city_name');
             $table->string('city_population');
