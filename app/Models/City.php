7ff20e31265a5e0a44d3a567652bcd1776ccddec
fmontenegro0510 <franciscomontenegro22@gmail.com>
2022-05-26 18:21:29 -0300

    fix database and added relations

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class City extends Model
 {
     use HasFactory;
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
--- database/migrations/2022_05_26_202750_create_cities_table.php
+++ database/migrations/2022_05_26_202750_create_cities_table.php
@@ -14,15 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('cities', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->unsignedInteger('country_id');
             $table->string('city_name');
             $table->string('city_population');
             $table->timestamps();
-
             $table->foreign('country_id')
                 ->references('id')
-                ->on('countries');
+                ->on('countries')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
